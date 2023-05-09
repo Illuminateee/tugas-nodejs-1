77e35de77f4b1b1e82b1b7ed7f636cb7c4e69767
Muhammad Ariq Akbar <ariqakbar.muhammad.ma16@gmail.com>
2023-05-10 01:54:41 +0700

    Tugas nodes 1

---
+++ main.js
@@ -0,0 +1,11 @@
+let mod = require('./moduleName');
+
+let name = 'Muhammad Ariq Akbar';
+let tempat = 'Bandung';
+let tgl = '13 September 2002';
+let alamat = 'Jalan Merkuri'
+
+console.log('Name : ' + mod.nama(name))
+console.log('Tempat Lahir : ' + mod.tempat(tempat))
+console.log('Tanggal Lahir : ' + mod.tgl(tgl))
+console.log('Alamat : ' + mod.alamat(alamat))
---
+++ moduleName.js
@@ -0,0 +1,15 @@
+exports.nama = (name) =>{
+    return name
+}
+
+exports.tempat = (tmpt) =>{
+    return tmpt;
+}
+
+exports.tgl = (tanggal) =>{
+    return tanggal;
+}
+
+exports.alamat = (almt) =>{
+    return almt;
+}
